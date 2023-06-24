53bd9a081077af954528da50c644f6514ceded82
Pedro Carneiro <pedrom.pm47@gmail.com>
2023-06-24 11:38:42 -0300

    incremento das opções de customização pelo usuário

--- html-flask-js-python-pedro/app.py
+++ html-flask-js-python-pedro/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, send_file
 import pandas as pd
 import matplotlib.pyplot as plt
-import io
 from chart_generator import generate_line_chart, generate_bar_chart
 
 app = Flask(__name__)
@@ -11,12 +10,17 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         chart_type = request.form['chart_type']
+        x_axis = request.form['x_axis']
+        y_axis = request.form['y_axis']
+        x_planilha = request.form['x_planilha']
+        y_planilha = request.form['y_planilha']
+
         df = pd.read_excel(file)
         
         if chart_type == 'line':
-            chart_buffer = generate_line_chart(df)
+            chart_buffer = generate_line_chart(df, eixo_x = x_axis, eixo_y = y_axis, x_planilha=x_planilha, y_planilha=y_planilha)
         elif chart_type == 'bar':
-            chart_buffer = generate_bar_chart(df)
+            chart_buffer = generate_bar_chart(df, eixo_x = x_axis, eixo_y = y_axis, x_planilha=x_planilha, y_planilha=y_planilha)
         else:
             return 'Tipo de gráfico inválido.'
         
--- html-flask-js-python-pedro/chart_generator.py
+++ html-flask-js-python-pedro/chart_generator.py
@@ -2,10 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 
-def generate_line_chart(df):
-    plt.plot(df['x'], df['y'])
-    plt.xlabel('X')
-    plt.ylabel('Y')
+def generate_line_chart(df, eixo_x = "X", eixo_y = "Y", x_planilha = "x", y_planilha = "y"):
+    plt.plot(df[x_planilha], df[y_planilha])
+    plt.xlabel(eixo_x)
+    plt.ylabel(eixo_y)
     
     img_buffer = io.BytesIO()
     plt.savefig(img_buffer, format='png')
@@ -15,10 +15,10 @@ def generate_line_chart(df):
     
     return img_buffer
 
-def generate_bar_chart(df):
-    plt.bar(df['z'], df['y'])
-    plt.xlabel('X')
-    plt.ylabel('Y')
+def generate_bar_chart(df, eixo_x = "X", eixo_y = "Y", x_planilha = "x", y_planilha = "y"):
+    plt.bar(df[x_planilha], df[y_planilha])
+    plt.xlabel(eixo_x)
+    plt.ylabel(eixo_y)
     
     img_buffer = io.BytesIO()
     plt.savefig(img_buffer, format='png')
