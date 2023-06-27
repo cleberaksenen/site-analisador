a21ae7279b159bce006e2feab242e85170bee652
cleberaksenen <cleber.aksenen@fiocruz.br>
2023-06-27 10:11:42 -0300

    Adição do gráfico boxplot

--- html-flask-js-python-pedro/app.py
+++ html-flask-js-python-pedro/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, send_file
 import pandas as pd
 import matplotlib.pyplot as plt
-from chart_generator import generate_line_chart, generate_bar_chart
+from chart_generator import generate_line_chart, generate_bar_chart, generate_box_chart
 
 app = Flask(__name__)
 
@@ -21,6 +21,8 @@ def upload_file():
             chart_buffer = generate_line_chart(df, eixo_x = x_axis, eixo_y = y_axis, x_planilha=x_planilha, y_planilha=y_planilha)
         elif chart_type == 'bar':
             chart_buffer = generate_bar_chart(df, eixo_x = x_axis, eixo_y = y_axis, x_planilha=x_planilha, y_planilha=y_planilha)
+        elif chart_type == 'box':
+            chart_buffer = generate_box_chart(df, eixo_x = x_axis, eixo_y = y_axis, x_planilha=x_planilha, y_planilha=y_planilha)
         else:
             return 'Tipo de gráfico inválido.'
         
--- html-flask-js-python-pedro/chart_generator.py
+++ html-flask-js-python-pedro/chart_generator.py
@@ -27,3 +27,16 @@ def generate_bar_chart(df, eixo_x = "X", eixo_y = "Y", x_planilha = "x", y_plani
     plt.clf()
     
     return img_buffer
+
+def generate_box_chart(df, eixo_x = "X", eixo_y = "Y", x_planilha = "x", y_planilha = "y"):
+    plt.boxplot(df[y_planilha])
+    plt.xlabel(eixo_x)
+    plt.ylabel(eixo_y)
+    
+    img_buffer = io.BytesIO()
+    plt.savefig(img_buffer, format='png')
+    img_buffer.seek(0)
+    
+    plt.clf()
+    
+    return img_buffer
